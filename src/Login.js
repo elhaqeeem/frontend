f084fa6e10a94e70b7c4b96aa9fdc8d56a760917
Lukmanul Hakim <manglayangselalu@gmail.com>
2024-11-16 16:29:56 +0700

    update func

--- src/Login.js
+++ src/Login.js
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion'; // Import library untuk animasi
 import 'react-toastify/dist/ReactToastify.css';
 import axiosInstance from './axiosInstance';
 
 const FeatureList = () => {
   const features = [
-    'Dark/light mode switch',
-    'Redux toolkit and other utility libraries set up',
-    'Calendar, Modal, Sidebar components included',
-    'User-friendly documentation provided',
-    'Daisy UI components and Tailwind CSS supported',
+    '🌙 Dark/light mode switch',
+    '⚡ Redux toolkit and utility libraries set up',
+    '📅 Calendar, Modal, Sidebar components included',
+    '📘 User-friendly documentation provided',
+    '✨ Daisy UI components and Tailwind CSS supported',
   ];
 
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
       <h1 className="text-2xl mt-8 font-bold">EduLMS Features</h1>
       {features.map((feature, index) => (
         <p key={index} className="py-2">
-          ✓ <span className="font-semibold" dangerouslySetInnerHTML={{ __html: feature }} />
+          <span className="font-semibold">{feature}</span>
         </p>
       ))}
-    </>
+    </motion.div>
   );
 };
 
@@ -30,30 +35,28 @@ const LoginForm = ({ onLogin }) => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
     setError('');
-  
-    console.log("Login initiated with:", { username, password }); // Debug data login
-  
-    axiosInstance.post('/login', { username, password })
-      .then(response => {
-        console.log("Login successful:", response.data); // Debug respons sukses
+    setIsLoading(true);
+
+    axiosInstance
+      .post('/login', { username, password })
+      .then((response) => {
         toast.success('Login successful!');
         localStorage.setItem('roleID', response.data.role_id);
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('id', response.data.id);
         localStorage.setItem('email', response.data.email);
         localStorage.setItem('firstName', response.data.firstName);
-  
+
         onLogin();
         navigate('/');
-        console.log("Navigation to '/' completed."); // Debug apakah navigasi berhasil
-              })
-      .catch(error => {
-        console.log("Login failed:", error.response || error.message); // Debug error respons
+      })
+      .catch((error) => {
         if (error.response) {
           setError(error.response.data.error);
           toast.error('Login Failed: ' + error.response.data.error);
@@ -61,12 +64,19 @@ const LoginForm = ({ onLogin }) => {
           setError('An error occurred. Please try again.');
           toast.error('Login Failed: An error occurred. Please try again.');
         }
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
-  
 
   return (
-    <div className="py-24 px-10">
+    <motion.div
+      className="py-24 px-10"
+      initial={{ opacity: 0, x: 50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.8 }}
+    >
       <h2 className="text-2xl font-semibold mb-2 text-center">Login</h2>
       <form onSubmit={handleLogin}>
         {error && <p className="text-center text-error mt-4">{error}</p>}
@@ -78,11 +88,8 @@ const LoginForm = ({ onLogin }) => {
             type="text"
             className="input input-bordered w-full"
             value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-              console.log("Username:", e.target.value); // Debug nilai username
-            }}
-                        required
+            onChange={(e) => setUsername(e.target.value)}
+            required
           />
         </div>
 
@@ -95,11 +102,8 @@ const LoginForm = ({ onLogin }) => {
               type={showPassword ? 'text' : 'password'}
               className="input input-bordered w-full"
               value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                console.log("Password:", e.target.value); // Debug nilai password
-              }}
-                            required
+              onChange={(e) => setPassword(e.target.value)}
+              required
             />
             <button
               type="button"
@@ -111,15 +115,15 @@ const LoginForm = ({ onLogin }) => {
           </div>
         </div>
 
-        <div className="text-right text-primary mt-2">
-          <a href="/forgot-password" className="text-sm hover:underline">
-            Forgot Password?
-          </a>
-        </div>
-
-        <button type="submit" className="btn mt-6 w-full btn-primary">
-          Access Dashboard
-        </button>
+        {isLoading ? (
+          <div className="flex justify-center mt-6">
+            <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"></div>
+          </div>
+        ) : (
+          <button type="submit" className="btn mt-6 w-full btn-primary">
+            Access Dashboard
+          </button>
+        )}
 
         <div className="text-center mt-4">
           Don't have an account yet?{' '}
@@ -128,35 +132,18 @@ const LoginForm = ({ onLogin }) => {
           </a>
         </div>
       </form>
-    </div>
+    </motion.div>
   );
 };
 
 const Login = ({ onLogin }) => {
   return (
-    <div className="min-h-screen bg-base-200 flex items-center">
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-indigo-500 flex items-center">
       <div className="card mx-auto w-full max-w-5xl shadow-xl">
         <div className="grid md:grid-cols-2 grid-cols-1 bg-base-100 rounded-xl">
           <div className="hero min-h-full rounded-l-xl bg-base-200">
             <div className="hero-content py-12">
-              <div className="max-w-md">
-                <h1 className="text-3xl text-center font-bold">
-                  <img
-                    src="https://img.freepik.com/free-vector/privacy-policy-concept-illustration_114360-7853.jpg?w=740&t=st=1705223766~exp=1705224366~hmac=7ca3fbf8bf0efe280c542a27ca99cb2fb49580f9e04f1e397409b5cd040e4862"
-                    className="w-12 inline-block mr-2 mask mask-circle"
-                    alt="EduLMS-logo"
-                  />
-                  EduLMS
-                </h1>
-                <div className="text-center mt-12">
-                  <img
-                    src="https://img.freepik.com/free-vector/computer-login-concept-illustration_114360-7962.jpg?w=740&t=st=1705223807~exp=1705224407~hmac=6a7e1b3d37caa6155fb689df5e4b86e598db633aaa841ac84a9576363f76c66d"
-                    alt="EduLMS Login"
-                    className="w-48 inline-block"
-                  />
-                </div>
-                <FeatureList />
-              </div>
+              <FeatureList />
             </div>
           </div>
           <LoginForm onLogin={onLogin} />
