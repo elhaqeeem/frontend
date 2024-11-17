4526c831bd1904b06d60fa0a25fad167efa269c8
Lukmanul Hakim <manglayangselalu@gmail.com>
2024-11-17 13:18:13 +0700

    update func

--- src/Layout-ori.jsx
+++ src/Layout-ori.jsx
@@ -262,6 +262,8 @@ const Layout = ({ children }) => {
 
     return (
         <div className="min-h-screen flex flex-col overflow-x-hidden">
+                        <ToastContainer />
+
             <Joyride
                 steps={tourSteps}
                 run={runTour}
