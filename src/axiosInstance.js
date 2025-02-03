7090858cea9e10d57ec44803e0f7eb50f20ed989
Lukmanul Hakim <manglayangselalu@gmail.com>
2025-02-03 19:47:14 +0700

    update cors

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,18 +2,24 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, // Update with your API URL
+    baseURL: process.env.REACT_APP_API_URL, 
+    withCredentials: true, // Tambahkan ini
 });
+
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(config => {
-    const token = localStorage.getItem('token'); // Replace with your method of retrieving the token
+    const token = localStorage.getItem('token');
     if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
+        console.log("Token added to request:", token);
+    } else {
+        console.log("No token found in localStorage");
     }
     return config;
 }, error => {
     return Promise.reject(error);
 });
+
 
 export default axiosInstance;
